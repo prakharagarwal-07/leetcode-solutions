59cf190bff2ccb16038890fe7755591b8eaa6515
prakharagarwal-07 <prakhar.agarwal.0710@gmail.com>
2026-07-31 02:49:19 +0530

    Time: 255 ms (52.02%), Space: 10.4 MB (81.64%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -4,10 +4,10 @@ public:
     vector<int> dr = { -1 , 0 , 1 , 0 };
     vector<int> dc = { 0 , 1 , 0 , -1 };
 
-    bool check( vector<vector<char>> &board , string &word , int row , int col , int idx , vector<vector<bool>> &visit ){
+    bool check( vector<vector<char>> &board , string &word , int row , int col , int idx  ){
 
 
-        if( row < 0 || col < 0 || row >= board.size() || col >= board[0].size() || visit[row][col] == true ){
+        if( row < 0 || col < 0 || row >= board.size() || col >= board[0].size()  ){
 
             return false;
 
@@ -32,14 +32,15 @@ public:
             return false;
         }
 
-        visit[row][col] = true;
+        char temp = board[row][col];
+        board[row][col] = '#';
 
         for( int i = 0 ; i < 4 ; i++ ){
 
             int nextRow = row + dr[i];
             int nextCol = col + dc[i];
 
-            if(check( board , word , nextRow , nextCol , idx , visit)){
+            if(check( board , word , nextRow , nextCol , idx )){
 
                 return true;
 
@@ -47,7 +48,7 @@ public:
 
         }
 
-        visit[row][col] = false;
+        board[row][col] = temp;
 
         return false;
 
@@ -61,14 +62,13 @@ public:
         int col;
         int idx = 0;
 
-        vector<vector<bool>> visit( board.size() , vector<bool>( board[0].size() , false ));
 
 
         for( row = 0 ; row < board.size() ; row++ ){
 
             for( col = 0; col < board[0].size() ; col++ ){
 
-                if( check( board , word , row , col , idx , visit )){
+                if( check( board , word , row , col , idx )){
 
                     return true;
                 }

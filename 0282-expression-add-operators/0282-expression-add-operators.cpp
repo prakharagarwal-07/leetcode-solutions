075a24fed9ef7fa3ec8fcc3154de63fd76644d2a
prakharagarwal-07 <prakhar.agarwal.0710@gmail.com>
2026-08-01 19:16:39 +0530

    Time: 410 ms (51.14%), Space: 95.5 MB (7.54%) - LeetHub

---
+++ 0282-expression-add-operators/0282-expression-add-operators.cpp
@@ -0,0 +1,112 @@
+class Solution {
+public:
+
+    long long int number( string segment ){
+
+        long long number = 0;
+
+        for( char val: segment ){
+
+            number = 1LL*number*10 + (val-'0');
+        }
+
+        return number;
+    }
+
+    vector<char> op = { '+' , '-' , '*' };
+
+    void combo( string &num , string curr , vector<string> &ans , int target , long long int sum , long long int lastOp , int i ){
+
+        if( i >= num.size() && target == sum ){
+
+            ans.push_back(curr);
+            return;
+        }
+
+        if( i >= num.size() ){
+
+            return;
+        }
+
+
+
+        for( int j = i ; j < num.size() ; j++ ){
+
+            string segment = num.substr( i , j-i+1 );
+
+            long long int value = number(segment);
+
+            if( j > i && num[i] == '0' ){
+
+                    break;
+                }
+
+
+            if( i == 0 ){
+
+                    combo( num , curr+segment , ans , target , sum+value , value , j+1 );
+                    continue;
+
+                }
+
+                
+
+            for( int idx = 0 ; idx < 3 ; idx++ ){
+
+                
+
+                if( i > 0 ){
+
+                    curr += op[idx];
+
+                }
+
+
+                if( idx == 0){
+
+                combo( num , curr+segment , ans , target , sum+value , value , j+1 );
+                
+
+                }
+
+                if( idx == 1 ){
+
+                combo( num , curr+segment , ans , target , sum-value , -value , j+1 );
+                
+
+
+                }
+
+                if( idx == 2 ){
+
+                combo( num , curr+segment , ans , target , sum - lastOp + lastOp*value , lastOp*value ,  j+1 );
+                
+
+
+                }
+
+                if( i > 0 ){
+
+                    curr.pop_back();
+
+                }
+
+
+            }
+        }
+    }
+    vector<string> addOperators(string num, int target) {
+
+        string curr;
+        vector<string> ans; 
+        long long int sum = 0;
+        int i = 0;
+        long long lastOp;
+
+    combo( num ,  curr , ans , target , sum , lastOp , i );
+
+    return ans;
+
+        
+    }
+};

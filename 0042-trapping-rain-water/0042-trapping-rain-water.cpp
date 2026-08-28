7e43162839f2cbbde14f1c09a2f868070b6f4cd9
Prakhar Agarwal <prakhar.agarwal.0710@gmail.com>
2026-08-28 23:58:50 +0530

    Time: 0 ms (100%), Space: 25.8 MB (99.75%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -4,33 +4,33 @@ public:
 
         int size = height.size();
 
-        vector<int> leftMax( size , 0 );
+        int left = 0;
+        int right = size-1;
 
-        leftMax[0] = height[0];
-
-        for( int i = 1 ; i < size ; i++ ){
-
-            leftMax[i] = max( leftMax[i-1] , height[i] );
-        }
-
-        vector<int> rightMax( size , 0 );
-
-        rightMax[size-1] = height[size-1];
-
-        for( int j = size - 2 ; j >= 0 ; j-- ){
-
-            rightMax[j] = max( rightMax[j+1] , height[j] );
-        }
+        int lMax = 0;
+        int rMax = 0;
 
         int water = 0;
 
+        while( left <= right ){
 
-        for( int k = 0 ; k < size ; k++ ){
+            lMax = max( height[left] , lMax );
+            rMax = max( height[right] , rMax );
 
-            water = water + ( min( leftMax[k] , rightMax[k] ) - height[k] );
+            if( lMax < rMax ){
 
+                water += lMax - height[left];
+                left++;
+            }
+            else{
 
+                water += rMax - height[right];
+                right--;
+            }
 
+            
+ 
+           
 
         }
 
